e54dca6f96079eaa4ac6ad9a03726f7a28e30c1f
toddjudd <todd.judd@gmail.com>
2022-06-28 17:52:28 -0600

    feat(db): prisma and pscale db
    set up db schema in the prisma folder
    set up planet scale db
    used pscale cli to `connect polls dev`
    used prisma to push schema to dev
    used pscale to pull dev to master
    show db records on the main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
-const Home: NextPage = () => {
-  return <div>Next JS!</div>;
+import { prisma } from '../db/client';
+
+export const getServerSideProps = async () => {
+  const questions = await prisma.pollQuestion.findMany();
+  return {
+    props: {
+      questions: JSON.stringify(questions, null, 2),
+    },
+  };
+};
+
+const Home: NextPage = (props: any) => {
+  return (
+    <div>
+      <Head>
+        <title>Polls</title>
+        <meta
+          name='description'
+          content='A polling app used to learn Next.js'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main>
+        <h1 className='text-2xl font-bold'>Welcome to the polls app</h1>
+        <code>{props.questions}</code>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
