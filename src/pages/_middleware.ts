4eec5263bb4ffc6ef87d7ffc54a89bb524901ff6
toddjudd <todd.judd@gmail.com>
2022-07-01 13:10:07 -0600

    feat:

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,1 +1,6 @@
-export {};
+import type { NextFetchEvent, NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest, ev: NextFetchEvent) {
+  console.log('Request: ', request);
+  console.log('Event: ', ev);
+}
