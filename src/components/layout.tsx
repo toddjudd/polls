7fdc948f1f00368d086c84017d955460a6080f3b
toddjudd <todd.judd@gmail.com>
2022-07-09 13:28:09 -0600

    feat(nav): :lipstick: Soda Vote
    updated nav. added side nave. set up sticky scrolling, moved links arround. removed title from layout to reduce state jumping around.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,43 +1,50 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { ReactNode } from 'react';
-import { HomeIcon } from '@heroicons/react/solid';
+import { HomeIcon, ChatAlt2Icon } from '@heroicons/react/solid';
 
 export const Layout: React.FC<{
-  title: string;
+  title?: string;
   children: ReactNode;
 }> = ({ title, children }) => {
   return (
     <div>
       <Head>
-        <title>{title}</title>
+        <title>{title ? `${title} | ` : ''}Soda Vote</title>
         <meta
           name='description'
           content='A polling app used to learn Next.js'
         />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <nav className='bg-zinc-600 m-8 rounded-lg flex flex-col'>
-        <div className='flex justify-between p-4'>
+      <nav className='sticky top-0 bg-zinc-700 border-b-[1px] border-b-zinc-300 z-50 h-16'>
+        <div className='flex gap-8 items-stretch font-bold h-full'>
           <Link rel='stylesheet' href='/'>
-            🏠
+            <span className='flex gap-2 text-4xl items-center'>
+              <ChatAlt2Icon className='h-10 w-10 text-amber-500' />
+              <span>Soda Vote</span>
+            </span>
           </Link>
-          <div className='text-xl font-bold'>{title}</div>
           <Link href='/poll/create'>
-            <div className='bg-zinc-300 text-sm text-zinc-600  p-2 rounded-md'>
-              Create Question
-            </div>
+            <span className='flex justify-center items-center'>
+              Create Poll
+            </span>
+          </Link>
+          <Link href='/'>
+            <span className='flex justify-center items-center'>View Poll</span>
           </Link>
         </div>
       </nav>
       <main className='grid grid-cols-[auto_1fr] justify-stretch max-w-6xl m-auto'>
-        <section className='w-[200px] flex felx-col p-4'>
-          <Link href='/'>
-            <span className='flex justify-center gap-4 w-full'>
-              <HomeIcon className='h-5 w-5 text-zinc-300' />
-              <span>Dashboard</span>
-            </span>
-          </Link>
+        <section className='block'>
+          <div className='w-[200px] flex felx-col p-4 sticky top-[69px]'>
+            <Link href='/'>
+              <span className='flex justify-center gap-4 w-full'>
+                <HomeIcon className='h-5 w-5 text-zinc-300' />
+                <span>Dashboard</span>
+              </span>
+            </Link>
+          </div>
         </section>
         <section>{children}</section>
       </main>
