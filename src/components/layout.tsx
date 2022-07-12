67e5fe1ea723458171003675997b380c3511852b
toddjudd <todd.judd@gmail.com>
2022-07-11 21:51:49 -0600

    style(auth): :lipstick: avatar
    put in drop menu when signed in

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,82 @@
-import { HomeIcon, ChatAlt2Icon } from '@heroicons/react/solid';
+import { Menu, Transition } from '@headlessui/react';
+import { HomeIcon, ChatAlt2Icon, LogoutIcon } from '@heroicons/react/solid';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import { ReactNode } from 'react';
 
+const AuthNav = () => {
+  const { data: session } = useSession();
+
+  if (!session)
+    return (
+      <div className='flex justify-center items-center '>
+        <button
+          onClick={() => signIn()}
+          className='bg-amber-500 px-2 py-1 rounded-md'>
+          Sign In
+        </button>
+      </div>
+    );
+
+  return (
+    <div className='flex justify-center items-center rounded-full border-2 border-amber-500 self-center relative'>
+      <Menu>
+        {({ open }) => (
+          <>
+            <Transition
+              show={open}
+              enter='transition ease-out duration-100'
+              enterFrom='transform opacity-0 scale-95'
+              enterTo='transform opacity-100 scale-100'
+              leave='transition ease-in duration-75'
+              leaveFrom='transform opacity-100 scale-100'
+              leaveTo='transform opacity-0 scale-95'
+              className='relative z-10'>
+              <Menu.Items
+                static
+                className='absolute top-[-16px] right-[-32px] w-40  origin-top-right bg-zinc-500 rounded-md shadow-lg outline-none z-10 -translate-y-2 translate-x-2'>
+                <Menu.Button className='px-4 py-3 flex justify-between w-full z-10 items-center'>
+                  <div className='text-xs'>{session.user?.name}</div>
+                  <div className='border-2 border-amber-500 h-[32px] w-[32px] rounded-full'>
+                    <Image
+                      src={session.user?.image || ''}
+                      height={32}
+                      width={32}
+                      alt='user image'
+                      className='rounded-full '
+                    />
+                  </div>
+                </Menu.Button>
+                <Menu.Button className='px-4 py-3 flex justify-between w-full z-10'>
+                  <p className='text-sm text-amber-500 font-medium leading-5 truncate'>
+                    Sign Out
+                  </p>
+                  <LogoutIcon className='h-5 w-5 text-amber-500' />
+                </Menu.Button>
+              </Menu.Items>
+            </Transition>
+            <Menu.Button className='h-[32px] w-[32px]'>
+              <Image
+                src={session.user?.image || ''}
+                height={32}
+                width={32}
+                alt='user image'
+                className='rounded-full border-2 border-amber-500'
+              />
+            </Menu.Button>
+          </>
+        )}
+      </Menu>
+    </div>
+  );
+};
+
 export const Layout: React.FC<{
   title?: string;
   children: ReactNode;
 }> = ({ title, children }) => {
-  const { data: session } = useSession();
   return (
     <div>
       <Head>
@@ -37,22 +104,7 @@ export const Layout: React.FC<{
             <span className='flex justify-center items-center'>View Polls</span>
           </Link>
           <span className='flex-1'></span>
-          {session ? (
-            <div className='flex  justify-center  items-center rounded-full border-[3px] border-amber-500 self-center'>
-              <Image
-                src={session.user?.image || ''}
-                height={50}
-                width={50}
-                alt='user image'
-                className='rounded-full border-2 border-amber-500'
-                onClick={() => {
-                  signOut();
-                }}
-              />
-            </div>
-          ) : (
-            <button onClick={() => signIn()}>Sign In</button>
-          )}
+          <AuthNav />
         </div>
       </nav>
       <main className='grid grid-colls-1 lg:grid-cols-[auto_1fr] justify-stretch max-w-7xl m-auto'>
